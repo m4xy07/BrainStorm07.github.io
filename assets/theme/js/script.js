88c8d45e4b099c60d44e148b544735f31fb148f0
BrainStorm07 <BrainStorm07@users.noreply.githhub.com>
2018-09-09 10:48:30 +0530

    Updates 😃😉
    👍👍

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,4 +1,3 @@
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise Website Builder</a> v4.8.2';document.body.insertBefore(a,document.body.childNodes[0])}}();
 (function($) {
 
     var isBuilder = $('html').hasClass('is-builder');
@@ -1003,3 +1002,4 @@
         }
     }
 })(jQuery);
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise Website Builder</a> v4.8.2';document.body.insertBefore(a,document.body.childNodes[0])}}();
